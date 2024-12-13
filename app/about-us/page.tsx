6e4bc08ecf6df0aed596f9ecc587dbbaab27ed2d
Hyunju Jennie Lee <hj.jennie.lee@gmail.com>
2024-12-13 14:22:42 -0700

    first commit

---
+++ app/about-us/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const metadata = {
+  title: "About us",
+};
+
+
+export default function AboutUs() {
+  return (
+    <div>
+      About Us
+    </div>
+  )
+}
---
+++ components/movie-info.tsx
@@ -0,0 +1,19 @@
+import { API_URL } from "../app/(home)/page";
+
+async function getMovies(id: string) {
+    console.log(`Fetching movies: ${Date.now()}`);
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    const response = await fetch(`${API_URL}/${id}`);
+    return response.json();
+  }
+  
+  export default async function MovieInfo({id} : {id: string}) {
+    const movie = await getMovies(id);
+  
+    return (
+      <div>
+        <h6>{JSON.stringify(movie)}</h6>
+      </div>
+    )
+  }
+  
